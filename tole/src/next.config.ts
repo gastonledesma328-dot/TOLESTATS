d31da81493b4afa36365ecb0a0c7b9ba5b42f505
gastonledesma328-dot <gastonledesma328@gmail.com>
2025-10-17 00:24:02 -0300

    Update next.config.ts

--- tole/src/next.config.ts
+++ tole/src/next.config.ts
@@ -1,27 +1,11 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        pathname: '/photos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        pathname: '/workspace-*/image/**',
-      },
-      { 
-        protocol: 'https', 
-        hostname: 'replicate.delivery', 
-        pathname: '/**', 
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    turbo: false
+  }
 }
 
-export default {}
-
-
+export default nextConfig
